5786d359d8fb679011eec485c28f77265f7c6c69
MahdiMirshafiee <Mirshafieemahdi001@gmail.com>
2025-12-07 12:18:58 +0330

    chess board component

---
+++ src/components/Board/Board.jsx
@@ -0,0 +1,119 @@
+import { useEffect, useRef, useState } from "react";
+import Pieces from "../Pieces/Pieces";
+import Files from "./bits/Files";
+import Ranks from "./bits/Ranks";
+import "./Board.css";
+import musicFile from "../../assets/Chess-music.mp3";
+
+function Board() {
+  const [theme, setTheme] = useState(document.cookie.split("=")[1] || "light");
+  const audioRef = useRef(null);
+  const [playing, setPlaying] = useState(false);
+
+  useEffect(() => {
+    document.cookie.split("=")[1] === theme
+      ? null
+      : (document.cookie = `theme=${theme};`);
+  }, [theme]);
+
+  const togglePlay = () => {
+    const audio = audioRef.current;
+    if (!audio) return;
+
+    if (playing) {
+      audio.pause();
+      setPlaying(false);
+    } else {
+      audio.play().catch((err) => console.warn("play() failed:", err));
+      setPlaying(true);
+    }
+  };
+
+  const ranks = Array(8)
+    .fill()
+    .map((x, i) => 8 - i);
+  const files = Array(8)
+    .fill()
+    .map((x, i) => i + 1);
+
+  const getClassName = (i, j) => {
+    let c = "tile";
+    theme === "light"
+      ? (c += (i + j) % 2 === 0 ? " tile--dark" : " tile--light")
+      : (c += (i + j) % 2 === 0 ? " tile--dark-d" : " tile--light-d");
+    return c;
+  };
+
+  return (
+    <>
+      <div className="controls">
+        <label className="ui-switch">
+          <input
+            type="checkbox"
+            checked={theme === "dark"}
+            onChange={() =>
+              setTheme((prev) => (prev === "light" ? "dark" : "light"))
+            }
+          />
+          <div className="slider">
+            <div className="circle"></div>
+          </div>
+        </label>
+
+        <div>
+          <audio ref={audioRef} src={musicFile} loop />
+
+          <button
+            onClick={() => togglePlay()}
+            className="music"
+            style={{
+              background: theme === "dark" ? "#80a1b5" : "#b48764",
+            }}
+          >
+            {playing ? (
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                width="24"
+                height="24"
+                fill="white"
+                viewBox="0 0 24 24"
+              >
+                <rect x="6" y="4" width="4" height="16" />
+                <rect x="14" y="4" width="4" height="16" />
+              </svg>
+            ) : (
+              <svg
+                style={{ paddingLeft: "3px" }}
+                xmlns="http://www.w3.org/2000/svg"
+                width="24"
+                height="24"
+                fill="white"
+                viewBox="0 0 24 24"
+              >
+                <polygon points="5,3 19,12 5,21" />
+              </svg>
+            )}
+          </button>
+        </div>
+      </div>
+
+      <div className="board">
+        <Ranks ranks={ranks} theme={theme} />
+        <div className="tiles">
+          {ranks.map((rank, i) =>
+            files.map((file, j) => (
+              <div
+                key={file + "-" + rank}
+                className={getClassName(9 - i, j)}
+              ></div>
+            ))
+          )}
+        </div>
+        <Pieces />
+        <Files files={files} theme={theme} />
+      </div>
+    </>
+  );
+}
+
+export default Board;
