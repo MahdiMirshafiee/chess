ba09144ab6bb474d198900806dfa5b1f23ac7d03
MahdiMirshafiee <Mirshafieemahdi001@gmail.com>
2025-12-16 16:21:34 +0330

    action close and open popup promotion

--- src/reducer/reducer.js
+++ src/reducer/reducer.js
@@ -1,3 +1,4 @@
+import { Status } from "../constant";
 import actionTypes from "./actions/actionTypes";
 
 export const reducer = (state, action) => {
@@ -25,6 +26,20 @@ export const reducer = (state, action) => {
       return {
         ...state,
         candidateMoves: [],
+      };
+    }
+    case actionTypes.PROMOTION_OPEN: {
+      return {
+        ...state,
+        status: Status.promoting,
+        promotionSquare: { ...action.payload },
+      };
+    }
+    case actionTypes.PROMOTION_CLOSE: {
+      return {
+        ...state,
+        status: Status.ongoing,
+        promotionSquare: null,
       };
     }
     default:
